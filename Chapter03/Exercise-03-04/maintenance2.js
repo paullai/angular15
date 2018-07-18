a1d83c3c31695294acf10e68e836de599b160985
Paul Lai <plai@pl.com>
2018-07-18 15:10:07 -0400

    ch 03-05

---
+++ Chapter03/Exercise-03-04/maintenance2.js
@@ -0,0 +1,20 @@
+﻿angular.module('maintenance', ['ngRoute'])
+    .controller('adminCtrl', AdminCtrl)
+    .controller('mainCtrl', MainCtrl)
+    .controller('locationsCtrl', LocationsCtrl)
+    .controller('sitesCtrl', SitesCtrl)
+    .factory('currentSpot', currentSpot)
+  .config(function ($routeProvider) {
+    $routeProvider.when('/locations', {
+      templateUrl: 'views/locations.html'
+    });
+    $routeProvider.when('/sites', {
+      templateUrl: 'views/sites.html'
+    });
+    $routeProvider.otherwise({
+      templateUrl: 'views/main.html'
+    });
+  });
+
+function AdminCtrl($scope) {
+}
